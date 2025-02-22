a7f5b14d7e1a7f0197b1ce2572aeb5af9da09b68
Seeridia <Seeridia@gmail.com>
2025-02-22 20:18:39 +0800

    增加签到 api

--- lib/routes-config.ts
+++ lib/routes-config.ts
@@ -18,6 +18,7 @@ export const ROUTES: EachRoute[] = [
       { title: "revocationAppointApp", href: "/revocationAppointApp" },
       { title: "queryMyAppoint", href: "/queryMyAppoint" },
       { title: "queryStationStatusByTime", href: "/queryStationStatusByTime" },
+      { title: "Seat Sign In", href: "/seatSignIn" },
     ],
   },
   {
@@ -27,7 +28,6 @@ export const ROUTES: EachRoute[] = [
     items: [
       { title: "Seat ID Reference Table", href: "/seatIdReferenceTable" },
       { title: "Seat ID Conversion Tool", href: "/seatIdConversionTool" },
-      { title: "Seat Sign In Code", href: "/seatSignInCode" },
     ],
   },
 ];
